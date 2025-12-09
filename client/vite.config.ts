e5746ecbae7a877bc03e835c1d87365011629b28
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2025-12-09 21:50:38 +0500

    configuring vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    server: {
+      port: 3000,
+      host: true,
+      watch: {
+        usePolling: true
       },
-    }),
-  ],
+      esbuild: {
+        target: "esnext",
+        platform: "linux"
+      },
+    },
+  }
 })
